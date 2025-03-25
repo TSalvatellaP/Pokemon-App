6948866b03984f72de9c8deb0cbd708d148c5352
Tania Salvatella <tsalvatellap@gmail.com>
2025-03-25 17:45:37 +0100

    redux devtools

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import { legacy_createStore as createStore } from 'redux';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const store = createStore(pokemonsReducer);
+const store = createStore(pokemonsReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 
 root.render(
